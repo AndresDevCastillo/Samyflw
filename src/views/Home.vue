8ccfa7c6672f180175691571ac175f9f89824f80
Andres Castillo <Andressempai03@gmail.com>
2024-02-17 18:45:17 -0500

    termine primera entrega

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <NavBar></NavBar>
   <div class="inicio">
     <div class="bg">
-      <div class="bubbles">
+      <div class="bubbles bubblesP">
         <span style="--i:11;"></span>
         <span style="--i:22;"></span>
         <span style="--i:24;"></span>
@@ -66,7 +66,61 @@
         <span style="--i:26;"></span>
         <span style="--i:17;"></span>
         <span style="--i:13;"></span>
+        <span style="--i:28;"></span>{ <span style="--i:22;"></span>
+        <span style="--i:24;"></span>
+        <span style="--i:18;"></span>
+        <span style="--i:14;"></span>
+        <span style="--i:23;"></span>
+        <span style="--i:18;"></span>
+        <span style="--i:16;"></span>
+        <span style="--i:19;"></span>
+        <span style="--i:20;"></span>
+        <span style="--i:22;"></span>
+        <span style="--i:25;"></span>
+        <span style="--i:18;"></span>
+        <span style="--i:21;"></span>
+        <span style="--i:13;"></span>
+        <span style="--i:15;"></span>
+        <span style="--i:26;"></span>
+        <span style="--i:17;"></span>
+        <span style="--i:13;"></span>
         <span style="--i:28;"></span>
+        <span style="--i:11;"></span>
+        <span style="--i:22;"></span>
+        <span style="--i:24;"></span>
+        <span style="--i:18;"></span>
+        <span style="--i:14;"></span>
+        <span style="--i:23;"></span>
+        <span style="--i:18;"></span>
+        <span style="--i:16;"></span>
+        <span style="--i:19;"></span>
+        <span style="--i:20;"></span>
+        <span style="--i:22;"></span>
+        <span style="--i:25;"></span>
+        <span style="--i:18;"></span>
+        <span style="--i:21;"></span>
+        <span style="--i:13;"></span>
+        <span style="--i:15;"></span>
+        <span style="--i:26;"></span>
+        <span style="--i:17;"></span>
+        <span style="--i:13;"></span>
+        <span style="--i:28;"></span>
+        <span style="--i:19;"></span>
+        <span style="--i:20;"></span>
+        <span style="--i:22;"></span>
+        <span style="--i:25;"></span>
+        <span style="--i:18;"></span>
+        <span style="--i:21;"></span>
+        <span style="--i:13;"></span>
+        <span style="--i:15;"></span>
+        <span style="--i:26;"></span>
+        <span style="--i:17;"></span>
+        <span style="--i:13;"></span>
+        <span style="--i:28;"></span>
+        <span style="--i:19;"></span>
+        <span style="--i:20;"></span>
+        <span style="--i:22;"></span>
+        <span style="--i:25;"></span>
       </div>
     </div>
     <div class="fondo">
@@ -84,7 +138,7 @@
         </div>
       </section>
       <div class="bg">
-        <div class="bubbles">
+        <div class="bubbles bubblesP">
           <span style="--i:11;"></span>
           <span style="--i:22;"></span>
           <span style="--i:24;"></span>
@@ -288,6 +342,7 @@ html {
 .bubbles {
   position: relative;
   display: flex;
+  justify-content: space-between;
 }
 
 .bubbles span {
